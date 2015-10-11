448bcee5936a1da1c9dd5a36a260f665ff123ab0
Andreas Madsack <andreas@madflex.de>
2015-10-11 21:53:37 +0200

    add a what to do.

--- client.py
+++ client.py
@@ -1,6 +1,12 @@
 import requests
 from config import API_TOKEN
 
+# TODO
+# - create index in elastic
+# - add schema for dataset using elasticsearch_dsl
+# - add every sensordata-set with values normalized to fields to elastic
+# - before doing http-call get newest dataset from elastic to speedup query to feinstaub-api by using timestamp_newer-GET-parameter
+
 
 def get_data():
     header = {'Authorization': 'Token ' + API_TOKEN}
